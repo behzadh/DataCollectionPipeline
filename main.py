2081054afb9e5ac4a619643c1032880e49380291
behzadh <behzad4041@gmail.com>
2022-08-30 17:08:34 +0100

    Updates main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from utils.ikea import DataCollection
 
 if __name__ == '__main__':
     get_url = 'https://www.ikea.com/gb/en/'
-    scrp = DataCollection(get_url)
+    scrp = DataCollection(get_url, headless=True) # if True, it works w/o opening the Chrome
     scrp.scrape_data()
